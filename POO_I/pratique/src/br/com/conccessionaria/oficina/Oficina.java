c498335c7e2d9fafef15c55f89705e874af972bf
f2lpOliveira <contato@fellipefreire.com.br>
2023-04-08 09:17:53 -0300

    Parâmetros do Construtor da Classe Oficina

--- POO_I/pratique/src/br/com/conccessionaria/oficina/Oficina.java
+++ POO_I/pratique/src/br/com/conccessionaria/oficina/Oficina.java
@@ -1,11 +1,23 @@
 package br.com.conccessionaria.oficina;
 
+import java.util.ArrayList;
+
+import br.com.conccessionaria.estrutura.Peca;
+import br.com.conccessionaria.funcionarios.Mecanico;
+import br.com.conccessionaria.veiculos.Veiculo;
+
 public class Oficina {
 
+	// Atributos da Classe
 	private String nomeOficina;
+	private ArrayList<Mecanico> listaMecanicos;
+	private ArrayList<Peca> listaPecas;
+	private ArrayList<Veiculo> listaVeiculos;
+	private int pecasNecessarias;
 
-	public String exibeNomeDaOficina (String nomeOficina){
-		return "";
+	// Construtor da Classe
+	public Oficina (String nomeOficina, int pecasNecessarias){
+		this.nomeOficina = nomeOficina;
+		this.pecasNecessarias = pecasNecessarias;
 	}
-	
 }
