4867475ab66f6834fabf00ace3627da9cc19e30a
f2lpOliveira <contato@fellipefreire.com.br>
2023-04-08 09:16:43 -0300

    Getters e Setters, Parâmetros do Construtor da Classe Peca

--- POO_I/pratique/src/br/com/conccessionaria/estrutura/Peca.java
+++ POO_I/pratique/src/br/com/conccessionaria/estrutura/Peca.java
@@ -2,17 +2,36 @@ package br.com.conccessionaria.estrutura;
 
 public class Peca {
 
+	// Atributos da Classe
 	private String nome;
 	private float valor;
 	private String dataCompra;
 
+	// Getters e Setters (Definição e Recuperação de Valores dos Atributos da Classe)
 	public String getNome() {
 		return this.nome;
 	}	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 	public float getValor() {
 		return this.valor;
 	}	
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
 	public String getDataCompra() {
 		return this.dataCompra;
 	}	
+	public void setDataCompra(String dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+
+	// Construtor da Classe
+	public Peca (String nome, float valor, String dataCompra){
+		this.nome = nome;
+		this.valor = valor;
+		this.dataCompra = dataCompra;
+	}
+
 }
