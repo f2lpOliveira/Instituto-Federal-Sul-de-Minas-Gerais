b0e18e9f8a9f1d689d0369cb4d95cfc3c64b9fcf
f2lpOliveira <contato@fellipefreire.com.br>
2023-04-08 13:30:10 -0300

    Getters e Setters, Definição de Métodos da Classe Oficina

--- POO_I/pratique/src/br/com/concessionaria/oficina/Oficina.java
+++ POO_I/pratique/src/br/com/concessionaria/oficina/Oficina.java
@@ -15,8 +15,69 @@ public class Oficina {
 	private ArrayList<Veiculo> listaVeiculos;
 	private int pecasNecessarias;
 
+	// Getters e Setters (Definição e Recuperação de Valores dos Atributos da Classe)
+	public String getNomeOficina() {
+		return this.nomeOficina;
+	}
+	public void setNomeOficina(String nomeOficina) {
+		this.nomeOficina = nomeOficina;
+	}
+	public ArrayList<Mecanico> getListaMecanicos() {
+		return this.listaMecanicos;
+	}
+	public void setListaMecanicos(ArrayList<Mecanico> listaMecanicos) {
+		this.listaMecanicos = listaMecanicos;
+	}
+	public ArrayList<Peca> getListaPecas() {
+		return this.listaPecas;
+	}
+	public void setListaPecas(ArrayList<Peca> listaPecas) {
+		this.listaPecas = listaPecas;
+	}
+	public ArrayList<Veiculo> getListaVeiculos() {
+		return this.listaVeiculos;
+	}
+	public void setListaVeiculos(ArrayList<Veiculo> listaVeiculos) {
+		this.listaVeiculos = listaVeiculos;
+	}
+	public int getPecasNecessarias() {
+		return this.pecasNecessarias;
+	}
+	public void setPecasNecessarias(int pecasNecessarias) {
+		this.pecasNecessarias = pecasNecessarias;
+	}
+
 	// Construtor da Classe
 	public Oficina (String nomeOficina){
 		this.nomeOficina = nomeOficina;
 	}
+
+	public void adicionarMecanico(Mecanico mecanico){
+		this.listaMecanicos.add(mecanico);
+	}
+	public void removerMecanico(Mecanico mecanico){
+		this.listaMecanicos.remove(mecanico);
+	}
+	public void adicionarPeca(Peca peca){
+		this.listaPecas.add(peca);
+	}
+	public void removerPeca(Peca peca){
+		this.listaPecas.remove(peca);
+	}
+	public void adicionarVeiculo(Veiculo veiculo){
+		this.listaVeiculos.add(veiculo);
+	}
+	public void removerVeiculo(Veiculo veiculo){
+		this.listaVeiculos.remove(veiculo);
+	}
+	public int verificarPecasRevisao(){
+		return 0;
+	}
+	public boolean realizarRevisaoVeiculos(){
+		return true;
+	}
+	public float valorTotalPecas(){
+		return 0;
+	}
+
 }
