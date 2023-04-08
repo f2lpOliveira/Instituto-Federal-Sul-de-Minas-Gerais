275305f62db4605b385e886f8c95a9e4e0ed346d
f2lpOliveira <contato@fellipefreire.com.br>
2023-04-08 09:14:43 -0300

    Getters e Setters, Parâmetros do Construtor da Classe Veiculo

--- POO_I/pratique/src/br/com/conccessionaria/veiculos/Veiculo.java
+++ POO_I/pratique/src/br/com/conccessionaria/veiculos/Veiculo.java
@@ -2,52 +2,60 @@ package br.com.conccessionaria.veiculos;
 
 public class Veiculo {
 
+	// Atributos da Classe
 	private String placa;
 	private int ano;
 	private int modelo;
 	private String numeroChassi;
 	private String cor;
 	private int kmRodados;
-
+	
+	// Getters e Setters (Definição e Recuperação de Valores dos Atributos da Classe)
 	public String getPlaca() {
 		return this.placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
 	}
 	public int getAno() {
 		return this.ano;
 	}
-	public int getModelo() {
-		return this.modelo;
-	}
-	public String getNumeroChassi() {
-		return this.numeroChassi;
-	}
-	public String getCor() {
-		return this.cor;
-	}
-	public int getKmRodados() {
-		return this.kmRodados;
-	}	
-
-	public void setPlaca(String placa) {
-		this.placa = placa;
-	}
 	public void setAno(int ano) {
 		this.ano = ano;
+	}
+	public int getModelo() {
+		return this.modelo;
 	}
 	public void setModelo(int modelo) {
 		this.modelo = modelo;
 	}
+	public String getNumeroChassi() {
+		return this.numeroChassi;
+	}
 	public void setNumeroChassi(String numeroChassi) {
 		this.numeroChassi = numeroChassi;
+	}
+	public String getCor() {
+		return this.cor;
 	}
 	public void setCor(String cor) {
 		this.cor = cor;
 	}
+	public int getKmRodados() {
+		return this.kmRodados;
+	}	
 	public void setKmRodados(int kmRodados) {
 		this.kmRodados = kmRodados;
 	}
 
-	public String exibeInformacoesDoVeiculo (String placa, int ano, int modelo, String numeroChassi, String cor, int kmRodados){
-		return "";
+	// Construtor da Classe
+	public Veiculo (String placa, int ano, int modelo, String numeroChassi, String cor, int kmRodados){
+		this.placa = placa;
+		this.ano = ano;
+		this.modelo = modelo;
+		this.numeroChassi = numeroChassi;
+		this.cor = cor;
+		this.kmRodados = kmRodados;
 	}
+	
 }
