3d93bebcb027dd0a004e881c98bf8056f840f2a1
f2lpOliveira <contato@fellipefreire.com.br>
2023-04-08 09:15:40 -0300

    Getters e Setters, Parâmetros do Construtor da Classe Mecanico

--- POO_I/pratique/src/br/com/conccessionaria/funcionarios/Mecanico.java
+++ POO_I/pratique/src/br/com/conccessionaria/funcionarios/Mecanico.java
@@ -2,18 +2,36 @@ package br.com.conccessionaria.funcionarios;
 
 public class Mecanico {
 
+	// Atributos da Classe
 	private String nome;
 	private int anosExperiencia;
 	private int carrosSimultaneos;
 
+	// Getters e Setters (Definição e Recuperação de Valores dos Atributos da Classe)
 	public String getNome() {
 		return this.nome;
 	}	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 	public int getAnosExperiencia() {
 		return this.anosExperiencia;
 	}	
+	public void setAnosExperiencia(int anosExperiencia) {
+		this.anosExperiencia = anosExperiencia;
+	}
 	public int getCarrosSimultaneos() {
 		return this.carrosSimultaneos;
 	}	
+	public void setCarrosSimultaneos(int carrosSimultaneos) {
+		this.carrosSimultaneos = carrosSimultaneos;
+	}
+
+	// Construtor da Classe
+	public Mecanico (String nome, int anosExperiencia, int carrosSimultaneos){
+		this.nome = nome;
+		this.anosExperiencia = anosExperiencia;
+		this.carrosSimultaneos = carrosSimultaneos;
+	}
 
 }
